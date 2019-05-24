c0e6b85ef6ecdc7389a61603cddb63ca4544bef2
Lars Kåre Skjørestad <lars.kaare@skjorestad.com>
2019-05-24 13:17:44 +0200

    Adding alternative config

--- config/config.js
+++ config/config.js
@@ -12,6 +12,19 @@ exports.exporter = {
 };
 
 exports.excel = {
+    eventSheetName : 'Kalender',
+    eventRange : 'A11:K166',
+    eventColCheck: 4,
+    eventStartDateCol: 0,
+    eventStartTimeCol: 5,
+    eventNameCol: 4,
+    eventDescCol: 6,
+    eventLocationCol: 10,
+    eventShowMapLinkCol: 9,
+    eventTimeZone: 'Europe/Oslo'
+};
+
+exports.excelA1 = {
     eventSheetName : 'Events',
     eventRange : 'A5:F100',
     eventColCheck: 2,
